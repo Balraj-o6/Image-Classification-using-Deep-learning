9bd38feff9375ffb1e8242d2142e69ca9fba3e5e
Balraj <balrajkv666@gmail.com>
2025-11-09 21:45:47 +0530

    Add files via upload

---
+++ image_classifier_web/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
+app = Flask(__name__)
+model = load_model('cifar10_model.h5')
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+UPLOAD_FOLDER = 'static/uploaded_images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return 'No file uploaded', 400
+
+    file = request.files['file']
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(file_path)
+
+    # Preprocess the uploaded image
+    img = image.load_img(file_path, target_size=(32, 32))
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions)]
+
+    return render_template('result.html',
+                           image_path=file_path,
+                           predicted_class=predicted_class)
+
+if __name__ == '__main__':
+    app.run(debug=True)
